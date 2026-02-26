51e8a02509deb68a217f5f74aa83ce8509fceb12
Katielebr <katielebrito@gmail.com>
2026-02-26 16:00:09 -0400

    fix: aumento gigantesco do cabeçalho v26

--- sw.js
+++ sw.js
@@ -1,8 +1,8 @@
-const CACHE_NAME = 'ranking-app-v25';
+const CACHE_NAME = 'ranking-app-v26';
 const STATIC_ASSETS = [
     './',
     './index.html',
-    './css/style.css?v=25',
+    './css/style.css?v=26',
     './js/app.js?v=99',
     './manifest.json',
     './icons/icon-192.png',
