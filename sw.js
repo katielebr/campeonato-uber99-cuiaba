e28d945ff92090dee577af505893d38b873ee0e5
Katielebr <katielebrito@gmail.com>
2026-02-26 15:27:46 -0400

    fix: aumento significativo do espaçamento do header v24

--- sw.js
+++ sw.js
@@ -1,8 +1,8 @@
-const CACHE_NAME = 'ranking-app-v23';
+const CACHE_NAME = 'ranking-app-v24';
 const STATIC_ASSETS = [
     './',
     './index.html',
-    './css/style.css?v=23',
+    './css/style.css?v=24',
     './js/app.js?v=99',
     './manifest.json',
     './icons/icon-192.png',
